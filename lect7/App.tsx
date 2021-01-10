e59deb7fd60fcc3764a9669eb3026de4dc2f549b
lukasz@react.edu.pl <56275797+lukasz-reactedupl@users.noreply.github.com>
2021-01-10 22:20:14 +0100

    Added stack navigation

--- lect7/App.tsx
+++ lect7/App.tsx
@@ -1,34 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
 
-  const [count, setCount] = useState(0);
-  const onPress = () => setCount(prevCount => prevCount + 1);
+  function ScreenOne({ navigation }) {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Go to Screen Two"
+          onPress={() => navigation.navigate('Screen Two')}
+        />
+      </View>
+    );
+  }
 
-  // const addingMessagesToConsoleHandler = (message: string) => {
-  //   console.log(`Message reads: ${message}`);
-  // }
+  function ScreenTwo({ navigation }) {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Go to Screen One"
+          onPress={() => navigation.navigate('Screen One')}
+        />
+      </View>
+    );
+  }
+
+  const Stack = createStackNavigator();
 
   return (
-    <View style={styles.container}>
-      <View style={styles.countContainer}>
-        <Text>Count: {count}</Text>
-      </View>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={onPress}
-      >
-        <Text>Press Here</Text>       
-        <Image
-          style={styles.tinyLogo}
-          source={{
-            uri: 'https://reactnative.dev/img/tiny_logo.png',
-          }}
-        />
-      </TouchableOpacity>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Screen One">
+        <Stack.Screen name="Screen One" component={ScreenOne} />
+        <Stack.Screen name="Screen Two" component={ScreenTwo} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -36,20 +44,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: 'center',
     paddingHorizontal: 20
-  },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
-  },
-  countContainer: {
-    alignItems: "center",
-    padding: 10
-  },
-  tinyLogo: {
-    marginTop: 10,
-    width: 50,
-    height: 50,
-  },
+  }
 });
