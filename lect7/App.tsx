a3dba00bf122864dd4d1329ec63bfe715c1f9826
bartoszluka <bartoszluka1@gmail.com>
2021-01-13 09:45:11 +0100

    task 1 works

--- lect7/App.tsx
+++ lect7/App.tsx
@@ -1,64 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Image } from 'react-native';
-
-  const renderListItem = ({ item }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{item.name}</Text>
-      <Image
-        style={styles.flag}
-        source={{
-          uri: `https://www.countryflags.io/${item.alpha2Code}/flat/64.png`,
-        }}
-      />
-    </View>
-  );
+import {
+	SafeAreaView,
+	View,
+	FlatList,
+	StyleSheet,
+	Text,
+	StatusBar,
+	Image,
+	TouchableOpacity,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import CountryList from './CountryList';
+import DetailsScreen from './DetailsScreen';
 
 const App = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [countries, setCountries] = useState([]);
+	const Stack = createStackNavigator();
 
-
-  useEffect(() => {
-    fetch('https://restcountries.eu/rest/v2/all')
-      .then((response) => response.json())
-      .then((json) => setCountries(json))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
-
-  return (
-    <SafeAreaView style={styles.container}>
-      { isLoading ? <Text>Loading...</Text> :
-        <View>
-          <Text>Found {countries.length} countries</Text>
-          <FlatList
-            data={countries.slice(0, countries.length)}
-            renderItem={renderListItem}
-            keyExtractor={item => item.name}
-          />
-        </View>}
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: '#00bfff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 24,
-  },
-  flag: {
-    height: 64,
-    width: 64
-  }
-});
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name='Home'
+					component={CountryList}
+					options={{ title: 'Welcome' }}
+				/>
+				<Stack.Screen name='Details' component={DetailsScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
 
 export default App;
---
+++ lect7/DetailsScreen.tsx
@@ -0,0 +1,41 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const CountryDetails = ({ route, navigation }) => {
+	const { item } = route.params;
+	navigation.setOptions({ title: `${item.name} details` });
+	return (
+		<View>
+			<Text>{item.name}</Text>
+			<Text>{item.capital}</Text>
+			<Text>{item.region}</Text>
+			<Text>{item.nativeName}</Text>
+			<Text>{item.numericCode}</Text>
+			<Image
+				style={styles.flag}
+				source={{
+					uri: `https://www.countryflags.io/${item.alpha2Code}/flat/64.png`,
+				}}
+			/>
+		</View>
+	);
+};
+export default CountryDetails;
+
+const styles = StyleSheet.create({
+	item: {
+		backgroundColor: '#00bfff',
+		padding: 20,
+		marginVertical: 8,
+		marginHorizontal: 16,
+	},
+	title: {
+		fontSize: 24,
+	},
+	flag: {
+		height: 300,
+		width: 300,
+	},
+});
