d15d2af27c552deadae163a823e57d7dc134e01e
bartoszluka <bartoszluka1@gmail.com>
2021-01-23 14:13:16 +0100

    trying out animations

--- components/CountryList.tsx
+++ components/CountryList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
 	SafeAreaView,
 	View,
@@ -11,6 +11,7 @@ import {
 	TextInput,
 	NativeSyntheticEvent,
 	TextInputChangeEventData,
+	Animated,
 } from 'react-native';
 import styles from '../styles/styles';
 import CountryListItem from './CountryListItem';
@@ -21,6 +22,24 @@ function CountryList({ navigation }) {
 	const [searchString, setSearchString] = useState('');
 	const defaultFetchUrl = 'https://restcountries.eu/rest/v2/all';
 	const [fetchUrl, setFetchUrl] = useState(defaultFetchUrl);
+
+	const fadeAnim = useRef(new Animated.Value(0)).current;
+	const fadeIn = () => {
+		// Will change fadeAnim value to 1 in 5 seconds
+		Animated.timing(fadeAnim, {
+			toValue: 1,
+			duration: 500,
+			useNativeDriver: true,
+		}).start();
+	};
+
+	const fadeOut = () => {
+		Animated.timing(fadeAnim, {
+			toValue: 0,
+			duration: 500,
+			useNativeDriver: true,
+		}).start();
+	};
 
 	const fetchData = () => {
 		console.log('fetching');
--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -14,6 +14,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import CountryList from './CountryList';
 import DetailsScreen from './DetailsScreen';
+import Animations from './Animations';
 
 const HomeScreen = () => {
 	const Stack = createStackNavigator();
